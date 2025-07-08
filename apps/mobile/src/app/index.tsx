92cb88734c53d61ca34bf58f72966a07a6dbbc98
Prabhu K H <25prabhu10@gmail.com>
2025-07-07 23:43:00 -0400

    feat: add exp - react native project

---
+++ apps/mobile/src/app/index.tsx
@@ -0,0 +1,41 @@
+import { Stack, useNavigation } from 'expo-router'
+import { useEffect } from 'react'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+
+export default function Home() {
+  const navigation = useNavigation()
+
+  useEffect(() => {
+    navigation.setOptions({ headerShown: false })
+  }, [navigation])
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Stack.Screen
+          options={{
+            title: 'Home',
+            headerStyle: { backgroundColor: '#f4511e' },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
+        <Text>🍜Welcome to Masala and Curry🍛</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 50,
+    height: 50,
+  },
+})
